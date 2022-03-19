e1be970e6dd1a28e1a8e65fe174a6305de65513c
Parzival1608vonKatze <65299676+Parzival1608vonKatze@users.noreply.github.com>
2022-03-19 18:42:51 +0100

    disabled gtk detection

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir ~/ftch
-cp ftch/* ~/ftch
-echo alias ftch="cd ~/ftch && bash main.sh && cd" >> ~/.zshrc
-echo alias ftch="cd ~/ftch && bash main.sh && cd" >> ~/.bashrc
--- src/main.sh
+++ src/main.sh
@@ -127,16 +127,16 @@ ui="$(basename "${ui}")"
 
 
 # get GTK theming
-gtk_theme=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name= | sed "s/gtk-theme-name=/ /g")
+#gtk_theme=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-theme-name= | sed "s/gtk-theme-name=/ /g")
 
 # get gtk-icon-theme
-gtk_icons=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-icon-theme-name= | sed "s/gtk-icon-theme-name=/ /g")
+#gtk_icons=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-icon-theme-name= | sed "s/gtk-icon-theme-name=/ /g")
 
 # get gtk-font 
-gtk_font=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-font-name= | sed "s/gtk-font-name=/ /g")
+#gtk_font=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-font-name= | sed "s/gtk-font-name=/ /g")
 
 #get gtk-cursor-theme
-gtk_cursor=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-cursor-theme-name= | sed "s/gtk-cursor-theme-name=/ /g")
+#gtk_cursor=$(cat ~/.config/gtk-3.0/settings.ini | grep gtk-cursor-theme-name= | sed "s/gtk-cursor-theme-name=/ /g")
 
 # OUTPUT
 
